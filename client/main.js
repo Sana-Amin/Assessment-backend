cb4ad7607d1132b472c0fff4c5bac054cb0e85c5
Sana-Amin <amin.sana19@gmail.com>
2022-07-19 17:39:32 -0500

    Innital Commit

---
+++ client/main.js
@@ -0,0 +1,20 @@
+const complimentBtn = document.getElementById("complimentButton")
+const fortuneButton = document.getElementById('fortuneButton')
+
+const getCompliment = () => {
+    axios.get("http://localhost:4000/api/compliment/")
+        .then(res => {
+            const data = res.data;
+            alert(data);
+    });
+};
+const getFortune = () => {
+    axios.get("http://localhost:4000/api/fortune/")
+        .then(res => {
+            const data = res.data;
+            alert(data);
+    });
+}
+
+complimentBtn.addEventListener('click', getCompliment)
+fortuneButton.addEventListener('click', getFortune)
---
+++ server/controller.js
@@ -0,0 +1,25 @@
+module.exports = {
+
+    getCompliment: (req, res) => {
+        const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
+      
+        // choose random compliment
+        let randomIndex = Math.floor(Math.random() * compliments.length);
+        let randomCompliment = compliments[randomIndex];
+      
+        res.status(200).send(randomCompliment);
+    },
+    getFortune: (req, res) => {
+        const fortunes = ["A friend asks only for your time not your money.",
+            "A friend is a present you give yourself.",
+            "A gambler not only will lose what he has, but also will lose what he doesn’t have.",
+            "A golden egg of opportunity falls into your lap this month.",
+            "A good friendship is often more important than a passionate romance."];
+      
+        let randomIndex = Math.floor(Math.random() * fortunes.length);
+        let randomFortune = fortunes[randomIndex];
+      
+        res.status(200).send(randomFortune);
+    }
+
+}
